96c9d1e72de3f4259b18a63e8bf8c0af3282b7be
Mqrshie <120640441+Mqrshie@users.noreply.github.com>
2023-02-26 20:10:58 +0800

    me silly

--- src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
+++ src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
@@ -88,14 +88,12 @@ public class ModuleManager
         this.modules.add(new SelfFill());
         this.modules.add(new ArrowESP());
         this.modules.add(new FastExp());
-        this.modules.add(new NoRender());
         this.modules.add(new Freecam());
         this.modules.add(new PortalESP());
         this.modules.add(new DonkeyFinder());
         this.modules.add(new AutoTotem());
         this.modules.add(new HoleSnap());
         this.modules.add(new GreenText());
-        this.modules.add(new QueueSkip());
 
 
     }
